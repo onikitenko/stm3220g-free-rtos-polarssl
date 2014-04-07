0b50f2f1134c735cc4a15818d891c4c5d3857796
Alex Nikitenko <onikitenko@cogniance.com>
2014-04-07 13:24:50 +0300

    repo init

---
+++ Project/STM32F20x_StdPeriph_Examples/HASH/HASH_SHA1MD5_DMA/Src/main.c
@@ -0,0 +1,258 @@
+/**
+  ******************************************************************************
+  * @file    HASH/HASH_SHA1MD5_DMA/Src/main.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    13-March-2014
+  * @brief   This example provides a short description of HASH digest calculation
+  *          using SHA1 and MD5 example.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2014 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F2xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup HASH_SHA1MD5_DMA
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* HASH handler declaration */
+HASH_HandleTypeDef     HashHandle;
+
+#if defined ( __ICCARM__ ) /*!< IAR Compiler */
+  #pragma data_alignment=4   
+#endif
+__ALIGN_BEGIN const uint8_t aInput[] __ALIGN_END = "The ARM Cortex-M3-based STM32 F2 series uses ST s advanced 90 nm NVM process technology with the innovative adaptive real-time memory accelerator (ART Accelerator) and multi-layer bus matrix. This offers an unprecedented trade-off in price and performance.";
+
+__ALIGN_BEGIN static uint8_t aMD5Digest[16] __ALIGN_END;
+__ALIGN_BEGIN static uint8_t aExpectMD5Digest[16] __ALIGN_END = {0x08, 0x9b, 0x02, 0x87, 0x7f, 0x71, 0x52, 0xb3,
+                                                                 0x91, 0x65, 0x07, 0x26, 0x50, 0xaf, 0xea, 0x23
+                                                                };
+__ALIGN_BEGIN static uint8_t aSHA1Digest[20] __ALIGN_END;
+__ALIGN_BEGIN static uint8_t aExpectSHA1Digest[20] __ALIGN_END = {0x5c, 0xa6, 0x81, 0x5e, 0x19, 0xc9, 0x65, 0x3f,
+                                                                  0x1f, 0xa6, 0xfd, 0x9e, 0xa3, 0xbd, 0xb4, 0x0d,
+                                                                  0x43, 0x09, 0x64, 0x3d
+                                                                 };
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  /* STM32F2xx HAL library initialization:
+       - Configure the Flash prefetch, instruction and Data caches
+       - Configure the Systick to generate an interrupt each 1 msec
+       - Set NVIC Group Priority to 4
+       - Global MSP (MCU Support Package) initialization
+     */
+  HAL_Init();
+
+  /* Configure the system clock to 120 MHz */
+  SystemClock_Config();
+
+  /* Configure LED1, LED3 and LED4 */
+  BSP_LED_Init(LED1);
+  BSP_LED_Init(LED3);
+  BSP_LED_Init(LED4);
+
+  /****************************************************************************/
+  /******************************** SHA1 **************************************/
+  /****************************************************************************/
+  HAL_HASH_DeInit(&HashHandle);
+  HashHandle.Init.DataType = HASH_DATATYPE_8B;
+
+  if (HAL_HASH_Init(&HashHandle) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Start HASH computation using DMA transfert */
+  if (HAL_HASH_SHA1_Start_DMA(&HashHandle, (uint8_t *)aInput, strlen((char const *)aInput)) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Get the computed digest value */
+  if (HAL_HASH_SHA1_Finish(&HashHandle, aSHA1Digest, 0xFF) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Compare computed digest with expected one */
+  if (memcmp(aSHA1Digest, aExpectSHA1Digest, sizeof(aExpectSHA1Digest) / sizeof(aExpectSHA1Digest[0])) != 0)
+  {
+    Error_Handler();
+  }
+  else
+  {
+    BSP_LED_On(LED1);
+  }
+
+  /****************************************************************************/
+  /******************************** MD5 ***************************************/
+  /****************************************************************************/
+  HAL_HASH_DeInit(&HashHandle);
+  HashHandle.Init.DataType = HASH_DATATYPE_8B;
+
+  if (HAL_HASH_Init(&HashHandle) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Start HASH computation using DMA transfert */
+  if (HAL_HASH_MD5_Start_DMA(&HashHandle, (uint8_t *)aInput, strlen((char const *)aInput)) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Get the computed digest value */
+  if (HAL_HASH_MD5_Finish(&HashHandle, aMD5Digest, 0xFF) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Compare computed digest with expected one */
+  if (memcmp(aMD5Digest, aExpectMD5Digest, sizeof(aExpectMD5Digest) / sizeof(aExpectMD5Digest[0])) != 0)
+  {
+    Error_Handler();
+  }
+  else
+  {
+    BSP_LED_On(LED4);
+  }
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 120000000
+  *            HCLK(Hz)                       = 120000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 240
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 5
+  *            VDD(V)                         = 3.3
+  *            Flash Latency(WS)              = 3
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 240;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 5;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  /* Turn LED3 on */
+  BSP_LED_On(LED3);
+  while (1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
